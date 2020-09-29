58d651ddb69cf5aa88430dd8960c56cdc2a81bec
Fredrik <fredrikf2000@gmail.com>
2020-09-29 14:52:28 +0200

    wrote code from lesson video

--- js/script.js
+++ js/script.js
@@ -4,12 +4,16 @@ console.log(queryString);
 
 const params = new URLSearchParams(queryString);
 
-console.log(params)
+console.log(params);
 
 const id = params.get("id");
 
 console.log(id);
 
-const name = params.get("surname");
+const name = params.get("name");
 
-console.log(name)
+console.log(name);
+
+const surname = params.get("surname");
+
+console.log(surname);
